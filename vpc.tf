25298d3ced0a15ef69ad9645a020957c030f0519
lingaiah-ctu <sivalingapractise555@gmail.com>
2024-12-24 15:29:19 +0530

    tf aws vpc peering

--- vpc.tf
+++ vpc.tf
@@ -76,7 +76,7 @@ resource "aws_subnet" "database" { #first name is public[0], second anme is publ
 #In Terraform, the aws_db_subnet_group resource is used to designate a collection of subnets where an RDS instance can be provisioned.
 
 resource "aws_db_subnet_group" "default" {
-
+   #Name = "${local.resource_name}"
     subnet_ids = aws_subnet.database[*].id
 
     tags = merge(
